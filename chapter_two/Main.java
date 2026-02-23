9613d81d301757e01c5c7acf7754b8d60a37fc56
Thamsanqa Hadebe <thamsanqahadebe98@gmail.com>
2026-02-23 10:47:00 +0200

    Chapter 2: Using Variable and type declarations.

---
+++ chapter_two/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Thamsanqa";
+        int age = 28;
+        double height = 2.80;
+        boolean isStudent = true;
+
+        System.out.println("Hey, there! My name is " + name + ".");
+        System.out.println("I am " + age + " years old.");
+    }
+}
