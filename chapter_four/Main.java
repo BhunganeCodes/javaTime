bb0c5ca954ba40eb1d18ec4283d421806f8954ab
Thamsanqa Hadebe <thamsanqahadebe98@gmail.com>
2026-02-23 11:50:27 +0200

    Conditionals: Checking if number is greater than, less than or equal to zero

---
+++ chapter_four/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int number = -99;
+
+        if (number > 0) {
+            System.out.println("Number is greater than zero!");
+        } else if (number < 0) {
+            System.out.println("Number is less than zero!");
+        } else {
+            System.out.println("Number is equal to zero!");
+        }
+    }
+}
