740795ad15b6d52ca9942315626faa2f326e6100
Thamsanqa Hadebe <thamsanqahadebe98@gmail.com>
2026-02-23 11:43:32 +0200

    Operators: Calculating the area of a triangle

---
+++ chapter_three/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double width = 10.0;
+        double height = 3.00;
+
+        System.out.println(width * height);
+    }
+}
