b3c1326edb774dd052ed9eb5561c92d022bfd0c2
Thamsanqa Hadebe <thamsanqahadebe98@gmail.com>
2026-02-24 20:26:54 +0200

    Update: added char data type

--- chapter_two/Main.java
+++ chapter_two/Main.java
@@ -4,6 +4,7 @@ public class Main {
         int age = 28;
         double height = 2.80;
         boolean isStudent = true;
+        char grade = 'a';
 
         System.out.println("Hey, there! My name is " + name + ".");
         System.out.println("I am " + age + " years old.");
