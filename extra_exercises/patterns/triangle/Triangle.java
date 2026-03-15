508ab90d9adf7bc8d692e14e08f85023aa222276
Thamsanqa Hadebe <thamsanqahadebe98@gmail.com>
2026-03-15 20:52:13 +0200

    New: Right angled triangle pattern

---
+++ extra_exercises/patterns/triangle/Triangle.java
@@ -0,0 +1,21 @@
+// Right Triangle Pattern in Java
+import java.util.Scanner;
+
+public class Triangle {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("What is the height of your triangle?: ");
+        int height = scanner.nextInt();
+
+        for (int i = 0; i <= height; i++){
+            for (int j = 0; j < i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+        scanner.close();
+    }
+}
